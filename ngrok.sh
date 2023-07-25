260b19108771879329f9ae77c82e388124373d77
chris00222 <132053946+chris00222@users.noreply.github.com>
2023-07-25 12:51:37 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -4,5 +4,18 @@ echo "======================="
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
 wget -O ng.sh https://raw.githubusercontent.com/chris00222/sassa/main/GCngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
-./ngrok tcp --region us 3389 &>/dev/null &
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3389 &>/dev/null &
 echo "======================="
