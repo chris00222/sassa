4bdf91f1011145badc5c95ef92c52cbbe28fbe50
chris00222 <132053946+chris00222@users.noreply.github.com>
2023-07-25 11:41:06 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -2,7 +2,7 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/chris00222/sassa/main/GCngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
